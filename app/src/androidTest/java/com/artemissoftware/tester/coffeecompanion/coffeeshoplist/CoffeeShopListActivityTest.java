a87b6e73a6e16007027b77d8c4c7676daa57f382
gustavo <gustavo.maia@b2b.com.pt>
2020-04-30 18:17:57 +0100

    Espresso Idling Resources

--- app/src/androidTest/java/com/artemissoftware/tester/coffeecompanion/coffeeshoplist/CoffeeShopListActivityTest.java
+++ app/src/androidTest/java/com/artemissoftware/tester/coffeecompanion/coffeeshoplist/CoffeeShopListActivityTest.java
@@ -3,12 +3,19 @@ package com.artemissoftware.tester.coffeecompanion.coffeeshoplist;
 import android.content.Intent;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 
 import com.artemissoftware.tester.R;
+import com.artemissoftware.tester.coffeecompanion.coffeeshopdetail.CoffeeShopDetailActivity;
+import com.artemissoftware.tester.coffeecompanion.coffeeshoplist.idlingresource.CoffeeShopsIdlingResource;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -43,31 +50,57 @@ public class CoffeeShopListActivityTest {
 
     @Rule
     public IntentsTestRule<CoffeeShopListActivity> intentsTestRule = new IntentsTestRule<CoffeeShopListActivity>(CoffeeShopListActivity.class) {
-        @Override
-        protected Intent getActivityIntent() {
-
-            //Intent intent = super.getActivityIntent();
-            Intent intent = new Intent();
-
-            CoffeeShopListModel fakeModel = new CoffeeShopListModel(true);
-            fakeModel.setCoffeeShops(getFakeCoffeeShops());
-
-            intent.putExtra(CoffeeShopListActivity.COFFEE_SHOPS_BUNDLE_KEY, fakeModel);
-
-            return intent;
-        }
-
-        @NonNull
-        private List<CoffeeShop> getFakeCoffeeShops() {
-
-            List<CoffeeShop> coffeeShops = new ArrayList<>(10);
-            for (int i = 0; i < 10; i++) {
-                coffeeShops.add(CoffeeShop.fake(i));
-            }
-            coffeeShops.add(0, fakeCoffeeShop);
-            return coffeeShops;
-        }
+//Provides fake data so the api is not called
+//        @Override
+//        protected Intent getActivityIntent() {
+//
+//            //Intent intent = super.getActivityIntent();
+//            Intent intent = new Intent();
+//
+//            CoffeeShopListModel fakeModel = new CoffeeShopListModel(true);
+//            fakeModel.setCoffeeShops(getFakeCoffeeShops());
+//
+//            intent.putExtra(CoffeeShopListActivity.COFFEE_SHOPS_BUNDLE_KEY, fakeModel);
+//
+//            return intent;
+//        }
+//
+//        @NonNull
+//        private List<CoffeeShop> getFakeCoffeeShops() {
+//
+//            List<CoffeeShop> coffeeShops = new ArrayList<>(10);
+//            for (int i = 0; i < 10; i++) {
+//                coffeeShops.add(CoffeeShop.fake(i));
+//            }
+//            coffeeShops.add(0, fakeCoffeeShop);
+//            return coffeeShops;
+//        }
     };
+
+    @Nullable
+    private CoffeeShopsIdlingResource coffeeShopsIdlingResource;
+
+    @Before
+    public void setup() {
+        coffeeShopsIdlingResource = intentsTestRule.getActivity().getCoffeeShopsIdlingResource();
+        Espresso.registerIdlingResources(coffeeShopsIdlingResource);
+    }
+
+    @After
+    public void teardown() {
+        if (null != coffeeShopsIdlingResource) {
+            Espresso.unregisterIdlingResources(coffeeShopsIdlingResource);
+        }
+    }
+
+
+    @Test
+    public void testCoffeeShopClick_opensCoffeeShopDetailActivity() {
+        onView(withId(R.id.coffee_shops_recycler))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        intended(hasComponent(CoffeeShopDetailActivity.class.getName()));
+    }
 
 
     @Test
