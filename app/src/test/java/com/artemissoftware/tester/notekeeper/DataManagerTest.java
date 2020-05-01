2bdaa84d2759948f00f896e97432d18707db5e1f
gustavo <gustavo.maia@b2b.com.pt>
2020-05-01 09:36:46 +0100

    Pre-processing Between Each Test

--- app/src/test/java/com/artemissoftware/tester/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/artemissoftware/tester/notekeeper/DataManagerTest.java
@@ -1,10 +1,20 @@
 package com.artemissoftware.tester.notekeeper;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DataManagerTest {
+
+    @Before
+    public void setUp() {
+
+        DataManager dm = DataManager.getInstance();
+        dm.getNotes().clear();
+        dm.initializeExampleNotes();
+    }
+
 
     @Test
     public void createNewNote() {
@@ -27,5 +37,35 @@ public class DataManagerTest {
         assertEquals(course, compareNote.getCourse());
         assertEquals(noteTitle, compareNote.getTitle());
         assertEquals(noteText, compareNote.getText());
+    }
+
+
+
+    @Test
+    public void findSimilarNotes() {
+
+        DataManager sDataManager = DataManager.getInstance();
+        final CourseInfo course = sDataManager.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText1 = "This is the body text of my test note";
+        final String noteText2  = "This is the body of my second test note";
+
+        int noteIndex1 = sDataManager.createNewNote();
+        NoteInfo newNote1 = sDataManager.getNotes().get(noteIndex1);
+        newNote1.setCourse(course);
+        newNote1.setTitle(noteTitle);
+        newNote1.setText(noteText1);
+
+        int noteIndex2 = sDataManager.createNewNote();
+        NoteInfo newNote2 = sDataManager.getNotes().get(noteIndex2);
+        newNote2.setCourse(course);
+        newNote2.setTitle(noteTitle);
+        newNote2.setText(noteText2);
+
+        int foundIndex1 = sDataManager.findNote(newNote1);
+        assertEquals(noteIndex1, foundIndex1);
+
+        int foundIndex2 = sDataManager.findNote(newNote2);
+        assertEquals(noteIndex2, foundIndex2);
     }
 }
