1e0ef99b345021edfd6a398e6854cc2dbc70f749
gustavo <gustavo.maia@b2b.com.pt>
2020-05-01 09:06:53 +0100

    Notekeeper package

--- app/src/main/java/com/artemissoftware/tester/MainActivity.java
+++ app/src/main/java/com/artemissoftware/tester/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.artemissoftware.tester.coffeecompanion.beveragelist.BeverageListActivity;
+import com.artemissoftware.tester.notekeeper.NoteListActivity;
 import com.artemissoftware.tester.tasklist.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, BeverageListActivity.class);
+        Intent intent = new Intent(this, NoteListActivity.class);
+
+        //Intent intent = new Intent(this, BeverageListActivity.class);
         //Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
