799f3c18fcc0a139b916b6d10dee741f955917af
gustavo <gustavo.maia@b2b.com.pt>
2020-05-01 09:50:33 +0100

    Test Drive Development

--- app/src/main/java/com/artemissoftware/tester/notekeeper/DataManager.java
+++ app/src/main/java/com/artemissoftware/tester/notekeeper/DataManager.java
@@ -189,6 +189,13 @@ public class DataManager {
 
         return new CourseInfo("java_core", "Java Fundamentals: The Core Platform", modules);
     }
+
+    public int createNewNote(CourseInfo course, String noteTitle, String noteText) {
+
+        NoteInfo note = new NoteInfo(course, noteTitle, noteText);
+        mNotes.add(note);
+        return mNotes.size() - 1;
+    }
     //endregion
 
 }
