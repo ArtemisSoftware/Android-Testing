34687f524dbcf7cfd42aa35d8d97991ca6c05e22
gustavo <gustavo.maia@b2b.com.pt>
2020-05-01 10:05:20 +0100

    Setting up the instrumented test

---
+++ app/src/androidTest/java/com/artemissoftware/tester/notekeeper/NoteCreationTest.java
@@ -0,0 +1,17 @@
+package com.artemissoftware.tester.notekeeper;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class NoteCreationTest {
+
+
+    @Rule
+    public ActivityTestRule<NoteListActivity> mNoteListActivityRule = new ActivityTestRule<>(NoteListActivity.class);
+}
